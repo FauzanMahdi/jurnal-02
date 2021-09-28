721ecaf865a1084ddcb6f4d62c9907b04a1249be
FauzanMahdi <fauzanmahdi93@gmail.com>
2021-09-28 22:07:14 +0700

    HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Fauzan Mahdi Irawan!");
+		System.out.println("NIM 6706213066!");
+	    System.out.println("Kelas RPLA4501!");
+		
+	}
+}
